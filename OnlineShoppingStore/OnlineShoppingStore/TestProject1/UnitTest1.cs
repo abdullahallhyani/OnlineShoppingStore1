2f847835fe91c7f7867f81b84b6e873a9b4e4f99
abdullah <1538363@uj.edu.sa>
2021-09-19 05:01:21 +0300

    creat test

--- OnlineShoppingStore/OnlineShoppingStore/TestProject1/UnitTest1.cs
+++ OnlineShoppingStore/OnlineShoppingStore/TestProject1/UnitTest1.cs
@@ -15,28 +15,38 @@ namespace TestProject1
 
             
             IWebDriver driver = new FirefoxDriver("./");
-            //driver.Url = "http://www.google.com";
-
-             //driver.Url = "https://www.dominos.sa/ar/pages/order/menu#!/menu/category/pizza/";
             driver.Manage().Window.Maximize();
-            //driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
-            //-- driver.FindElement(By.ClassName("media__image productImage")).Click();
-
-
-
-            //driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+  
+            driver.Url = "https://localhost:44328/Home/Index/";
             
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("1")).Click();
+            
+            driver.FindElement(By.Id("dropdown")).Click();
+           
+            driver.FindElement(By.Id("Checkout")).Click(); 
+            
+            driver.FindElement(By.Id("CheckoutDetails")).Click();
+            driver.FindElement(By.Id("name")).SendKeys("abdullah");
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
 
-            driver.Url = "https://www.dominos.sa/ar/pages/customer/#!/customer/login/";
-            //driver.Url = "https://localhost:44328/Home/Index/";
+            driver.FindElement(By.Id("CardNumber")).SendKeys("SA9020000000387992");
+            
+            driver.FindElement(By.Id("cvv")).SendKeys("012");
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Payment")).Click();
+            
+
+
 
             //driver.FindElement(By.LinkText("customerLoginPage"));
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
-            driver.FindElement(By.Id("Email")).SendKeys("abdullah.s.allhyani@gmail.com");
-            driver.FindElement(By.Id("Password")).SendKeys("Aa123456");
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
-            driver.FindElement(By.CssSelector("js-loginSubmit")).Submit();
+            //driver.FindElement(By.Id("Email")).SendKeys("abdullah.s.allhyani@gmail.com");
+            //driver.FindElement(By.Id("Password")).SendKeys("Aa123456");
+            //driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+            //driver.FindElement(By.CssSelector("js-loginSubmit")).Submit();
            // driver.FindElement(By.CssSelector("btn btn--small js-loginSubmit")).Click();
             
 
